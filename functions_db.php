fad3dbad11e88498e2de722a2ba798cf76acd183
Brian M. Herold <brian.herold@genesys.com>
2017-06-26 20:56:33 -0500

    check for board template int val vs boolean - we changed the field in the db to tinyint

--- functions_db.php
+++ functions_db.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/functions_mandrill.php';
 require_once __DIR__ . '/functions_util.php';
 
 function _db_getBoardTemplates() {
-  $board_rows = $GLOBALS['wpdb']->get_results( "SELECT * FROM pcw_boards WHERE template = 1 ORDER BY name" );
+  $board_rows = $GLOBALS['wpdb']->get_results( "SELECT * FROM pcw_boards WHERE template > 0 ORDER BY name" );
   if ($board_rows) return $board_rows;
 
   return false;
